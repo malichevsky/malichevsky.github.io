1639c35d07c06f6e55d55a0ad2927eaa4f4c991c
malichevsky <drogulpetr145@gmail.com>
2026-04-11 00:38:08 +0300

    secret sauce

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Portfolio;
+using Portfolio; //sybau
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#app"); //syfm
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
